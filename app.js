57627143159c7ad7e41d52d0abc08bbb9ff7da3b
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-04-12 15:52:25 +0300

    get add and remove

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const form = document.querySelector('.select-section');
+
 let sort = document.querySelector('.sort-down');
 let addBtn = document.querySelector('.button');
 let task = document.querySelector('input');
-let section = document.querySelector('.input-section')
-let clone = document.querySelector('.clone-input')
+let section = document.querySelector('.input-section');
+let clone = document.querySelector('.clone-input');
+let deleteBtn = document.querySelector('.delete');
 
 let arr = [];
-
+console.log(sort);
 sort.addEventListener('click', () => {
-    if ( sort.value === "1") {
+    if (sort.value === "1") {
         sort.style.background = "url('" + "/img/up-black.svg" + "')";
         sort.value = "2";
     } else {
@@ -17,21 +18,27 @@ sort.addEventListener('click', () => {
     }
 });
 
-addBtn.addEventListener('click', () => { 
+addBtn.addEventListener('click', () => {
     console.log(arr);
-    addTask() ;
+    addTask();
 });
 
 
-
-
 function addTask() {
-    arr.push({text:task .value})
-    let cloneSec = clone.cloneNode(true);
-    section.appendChild(cloneSec);
-    task .value = "";
+    arr.push({ text: task.value })
+    let addClone = clone.cloneNode(true);
+    section.appendChild(addClone);
+    addClone.querySelector('.delete').addEventListener('click', () => {
+        addClone.remove();
+    });
+    task.value = "";
     console.log(section);
 }
+
+deleteBtn.addEventListener('click', (event) => {
+    event.target.parentElement.remove();
+});
+
 
 
 
