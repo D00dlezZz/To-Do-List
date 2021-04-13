a37e449fabd0af9f748b4966100e525f49535e50
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-04-13 10:09:33 +0300

    revers sort

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ sort.addEventListener('click', () => {
     if (sort.value === "1") {
         sort.style.background = "url('" + "/img/up-black.svg" + "')";
         sort.value = "2";
+        render();
     } else {
         sort.style.background = "url('" + "/img/down-black.svg" + "')";
-        sort.value = "1";
+        sort.value = "1";   
     }
 });
 
@@ -26,8 +27,6 @@ addBtn.addEventListener('click', () => {
 
 
 function workWithTasks() {
-    arr.push({ text: task.value });
-    console.log(arr);
     let addClone = clone.cloneNode(true);
     section.appendChild(addClone);
     addClone.querySelector('.delete').addEventListener('click', () => {
@@ -37,20 +36,31 @@ function workWithTasks() {
         clone.remove();
     });
     task.value = "";
-    container.style.height = "1%";
-    secInp.style.height = "1%";
 }
 
+function render() {
+    let tasks = document.querySelectorAll('input');
+    tasks.forEach((item) => {
+        arr.push({text:item.value});
+    });
+    arr.sort((a, b) => {
+        if (a.text > b.text) {
+            return 1;
+        } else if (a.text < b.text) {
+            return -1;
+        } 
+        return 0;
+    });
+    console.log(arr);
 
-
-
-
-
-
-
-
-
-
-
+    section.innerHTML = '';
+    arr.forEach((item) => {
+        let newInput = document.createElement('div');
+        newInput.classList.add('clone-input');
+        newInput.innerHTML = `<input class="input" value = "${item.text}"type="text"><button class="delete"></button>`;
+        section.append(newInput);
+    });
+    arr = [];
+}
 
 
